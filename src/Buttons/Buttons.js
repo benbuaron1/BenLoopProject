f98de78c4cafdb7f6b9e41f02831d3c63c53b8d4
benbuaron1 <ben.buaron1@gmail.com>
2022-03-17 18:54:27 +0200

    Update for deploy

--- src/Buttons/Buttons.js
+++ src/Buttons/Buttons.js
@@ -1,14 +1,13 @@
 import {BsFillPauseFill, BsFillPlayFill, BsFillStopBtnFill} from "react-icons/bs";
-import {ImLoop} from "react-icons/im";
-import {CgArrowsHAlt} from "react-icons/cg";
+
 import React from "react";
 
 export default function Buttons(props) {
     return (
         <>
-            <button className='ButtonPlayer' onClick={props.togglePlayPause}>{props.isPlaying? <BsFillPauseFill/> : <BsFillPlayFill/>}</button>
-            <button className='ButtonPlayer' onClick={props.togglePause}><BsFillStopBtnFill/></button>
-            <button className='ButtonPlayer' onClick={props.toggleLoop}>{props.isLoop?<ImLoop/>  : <CgArrowsHAlt/>}</button><br/>
+            <button className='ButtonPlayer' onClick={props.togglePlayPause}>{props.isPlaying? 'Pause' : 'Play'}</button>
+            <button className='ButtonPlayer' onClick={props.togglePause}>Stop</button>
+            <button className='ButtonPlayer' onClick={props.toggleLoop}>{props.isLoop?'Loop' : 'UnLoop'}</button><br/>
         </>
     )
 }
